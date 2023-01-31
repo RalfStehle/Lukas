4c984c1282c589e1fdf940eb2cbdc435ca04b1ef
Ralf Stehle <ralf.stehle@gmx.de>
2023-01-31 22:16:32 +0100

    TEST: Neue Dialog-Box um den Servo zu justieren (SwitchFragment.java)

--- app/src/main/java/de/stehle/legoan/ui/SwitchFragment.java
+++ app/src/main/java/de/stehle/legoan/ui/SwitchFragment.java
@@ -1,16 +1,27 @@
 package de.stehle.legoan.ui;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.nio.charset.StandardCharsets;
 
@@ -23,7 +34,9 @@ public class SwitchFragment extends DeviceFragment {
     private final int contextMenuId = View.generateViewId();
     private final int setServoMenuId = View.generateViewId();
     private LayoutSwitchItemBinding binding;
-    private String servoSetting = "0x00 (byte)0x78";
+    private String servopos1 = "0";
+    private String servopos2 = "120";
+
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -59,7 +72,7 @@ public class SwitchFragment extends DeviceFragment {
             return true;
         }
         if (item.getItemId() == setServoMenuId) {
-            testServo();
+            setServoSetting();
             return true;
         }
 
@@ -76,15 +89,52 @@ public class SwitchFragment extends DeviceFragment {
         return (Switch) getDevice();
     }
 
-    // Test von Ralf
+    // Ergänzung von Ralf
+    private void setServoSetting() {
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.servo_dialog);
+
+        int width = (int)(getResources().getDisplayMetrics().widthPixels*0.90);
+        int height = (int)(getResources().getDisplayMetrics().heightPixels*0.50);
+
+        dialog.getWindow().setLayout(width, height);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.R.color.background_light));
+
+        EditText _servopos1 = (EditText) dialog.findViewById(R.id.servopos1);
+        EditText _servopos2 = (EditText) dialog.findViewById(R.id.servopos2);
+        Button btn_yes = (Button) dialog.findViewById(R.id.button1);
+        Button btn_cancel = (Button) dialog.findViewById(R.id.button2);
+
+        _servopos1.setInputType(InputType.TYPE_CLASS_NUMBER);
+        _servopos2.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        btn_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                servopos1 = _servopos1.getText().toString();
+                servopos2 = _servopos2.getText().toString();
+                String servoSetting = servopos1 + "#" + servopos2;
+                getSwitch().send(servoSetting.getBytes(StandardCharsets.UTF_8));
+                dialog.cancel();
+            }
+        });
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+        dialog.show();
+}
+
     private void testServo() {
         new ConfirmBuilder(requireActivity())
                 .setTitle(R.string.setup_servo)
                 .setConfirmText(R.string.send)
-                .setValue(servoSetting)
+                .setValue(servopos1)
                 .show(value -> {
-                    servoSetting = value;
-                    getSwitch().send(servoSetting.getBytes(StandardCharsets.UTF_8));
+                    servopos1 = value;
+                    getSwitch().send(servopos1.getBytes(StandardCharsets.UTF_8));
                 });
     }
 }
